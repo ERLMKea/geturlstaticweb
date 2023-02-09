c69ecf893c2e4dd15168dd4e3dcf20c8608ba27a
ERLMKea <erlm@kea.dk>
2023-02-09 17:03:58 +0100

    Initial commit

---
+++ js/fetchurl.js
@@ -0,0 +1,37 @@
+console.log("im in fetchurl")
+const urlRegioner = "https://api.dataforsyningen.dk/regioner";
+
+const inpUrl = document.getElementById("inpUrl")
+const pbFetch = document.getElementById("pbFetchUrl")
+const pbFetchRegioner = document.getElementById("pbFetchRegioner")
+const textArea = document.getElementById("txt")
+const pbClear = document.getElementById("pbClear")
+
+function fetchAnyUrlText(url) {
+    console.log("inside fetch json url=" + url)
+    return  fetch(url).then(response => response.text());
+}
+
+async function actionFetchUrl(btn) {
+    const url = inpUrl.value;
+    console.log(url)
+    const anybody = await fetchAnyUrlText(url)
+    console.log(anybody)
+    textArea.textContent = anybody
+}
+
+async function actionFetchRegioner(btn) {
+    console.log("fetch regioner")
+    const regioner = await fetchAnyUrlText(urlRegioner)
+    console.log(regioner)
+    textArea.textContent = regioner
+}
+
+function clearText() {
+    textArea.textContent = '';
+}
+
+pbFetchRegioner.addEventListener('click', actionFetchRegioner)
+pbFetch.addEventListener('click', actionFetchUrl)
+pbClear.addEventListener('click', clearText)
+
